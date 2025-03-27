f05fe3dd1864b8c65f8e5db2d1e9709086ca15d1
MidoriNoHoshi <dhruvamathisen@gmail.com>
2025-03-27 21:12:10 +0100

    edited batteryReveal.sh to look nicer

--- .config/hypr/scripts/batteryReveal.sh
+++ .config/hypr/scripts/batteryReveal.sh
@@ -1,27 +1,10 @@
 #!/usr/bin/env zsh
 
-# Charge = $(cat /sys/class/power_supply/BAT0/capacity)
-#
-# if $Charge < 10; then
-#   while true; do
-#     notify-send -u critical "$(cat /sys/class/power_supply/BAT0/capacity)"
-#     sleep 10
-#   done
-# fi
-
 if ($(cat /sys/class/power_supply/BAT0/capacity) < 10); then
   while true; do
-    notify-send -u critical "⚠️ Warning: $(cat /sys/class/power_supply/BAT0/capacity)%"
+    notify-send -u critical -a "Battery Percentage:" "⚠️ Warning: $(cat /sys/class/power_supply/BAT0/capacity)%"
     sleep 12
   done
 else;
-  notify-send "$(cat /sys/class/power_supply/BAT0/capacity)%"
+  notify-send -a "Battery Percentage:" "$(cat /sys/class/power_supply/BAT0/capacity)%"
 fi
-
-
-
-# notify-send "$(cat /sys/class/power_supply/BAT0/capacity)"
-
-# Charge = $(cat /sys/class/power_supply/BAT0/capacity)
-#
-# notify-send "⚠️ Warning: Battery at $Charge %. Save work before shutdown."
